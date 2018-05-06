6a2203162b106e186261aabe204a9c715953cb4f
Rafael Wolf de Goes <rewgoes@gmail.com>
2018-05-06 16:32:53 -0300

    feat: fetch contacts from server

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
+import * as ContactsApi from './utils/ContactsApi'
 
 class App extends Component {
   state = {
-    contacts: [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com", 
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: []
+  }
+  componentDidMount() {
+    ContactsApi.getAll().then((contacts) => {
+      this.setState({ contacts })
+    })
   }
   removeContact = (contact) => {
     this.setState((state) => ({
       contacts: state.contacts.filter((c) => c.id !== contact.id)
     }))
+
+    ContactsApi.remove(contact)
   }
   render() {
     return (
